52c28789a7031a2bf4e238949e8fac5cdfbf31f7
vietanhbkaaa <pham.viet.anh@framgia.com>
2016-05-14 23:25:00 +0700

    log-in-log-out

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   get 'static_pages/home'
@@ -6,6 +8,10 @@ Rails.application.routes.draw do
   get 'static_pages/help'
 
   get 'signup'  => 'users#new'
+
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 
   resources :users
 
